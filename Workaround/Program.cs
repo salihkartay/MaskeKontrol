e29122e7299cc467662442d4a91ef9c86ab4faf6
salihkartay <salih.kartay@mobim360.com>
2024-03-07 16:12:31 +0300

    commit1

---
+++ Workaround/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Person person1 = new Person();
+            person1.FirstName = "Salih";
+            person1.LastName = "Kartay";
+            person1.DateOfBirthYear = 1998;
+            person1.NationalIdentity = 123;
+
+            Person person2 = new Person { FirstName = "Fatih" };
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+
+            Console.ReadLine();
+        }
+
+       
+    }
+}
